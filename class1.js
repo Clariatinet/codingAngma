a278b1b32f7f934bfa01c34902a3e9b3ec02467c
sungmin <95615086+Clariatinet@users.noreply.github.com>
2023-12-17 23:25:22 +0900

    12/17

---
+++ class1.js
@@ -0,0 +1,10 @@
+const name = 'Mike';
+const age = 30;
+
+const name1 = 'Mike'; // 작은 따옴표
+const name2 = '"Mike"'; // 큰 따옴표
+const name3 = `Mike`; // 백틱
+
+const message = `My mom is ${name}`;
+
+console.log(message);
---
+++ class2.js
@@ -0,0 +1,19 @@
+const age = 30;
+const PI = 3.14;
+
+console.log(1 + 2); // 더하기
+console.log(10 - 3); // 빼기
+console.log(3 * 2); // 곱하기
+console.log(6 / 3); // 나누기
+console.log(6 % 4); // 나머지
+
+console.log(1 / 0);
+console.log('Mike' / 2);
+
+console.log(age === 30);
+console.log(age < 40);
+
+let user;
+console.log(user);
+user = null;
+console.log(user);
